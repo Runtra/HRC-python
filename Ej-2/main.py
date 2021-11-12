37064b16766aa2d79eb312f13f6fa8faa6ab1e52
Facu <50464463+Runtra@users.noreply.github.com>
2021-11-12 00:06:04 +0000

    ej 2 y 4

---
+++ Ej-2/main.py
@@ -0,0 +1,8 @@
+def main():
+    a = int(input("Ingrese un numero entero: "))
+    b = int(input("Ingrese otro numero entero: "))
+
+    print(f"{a} entre {b} da un cociente de {a//b} y un resto de {a%b}.")
+    
+if __name__ == "__main__":
+    main()
---
+++ Ej-4/main.py
@@ -0,0 +1,5 @@
+password = input("Introduce la contraseña: ")
+if password == "sesamo":
+    print("Pasa")
+else:
+    print("No pasa")
